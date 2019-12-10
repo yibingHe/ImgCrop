a901338665aab950ed854b6c1e3c2a4c950467f2
heyibing <240286230@qq.com>
2019-12-10 16:48:40 +0800

    .

--- README.md
+++ README.md
@@ -3,16 +3,20 @@
 ####版本号：1.0.0
 
 ####使用：
-1. 引入js
+1. 引入依赖文件 cropper、jquery、layer
+  - <link href="../src/cropperjs/cropper.min.css" rel="stylesheet">
+  - <script src="../src/jquery.min.js"></script>
+  - <script src="../src/cropperjs/cropper.min.js"></script>
+  - <script src="../src/layer/layer.js"></script>
+  - <script src="../src/CropImg.js"></script>
+
 2. 实例化 new CropImg(params1,params2)
 
 ```
 demo:
  new CropImg(url, {
-        step: { //宽高比
-            width: 16,
-            height: 9
-        },
+        stepWidth: 16, //宽高比
+        stepHeight: 9, //宽高比
         width: 320,//裁剪图片宽和高
         height: 200,
         isCircle: false,//是否预览显示为圆形
--- src/CropImg.js
+++ src/CropImg.js
@@ -1,94 +1,113 @@
-
-window.CropImg = (function () { 
-
-    /* 
-    依赖：
-    1. cropper、jquery、layer
-    实例化参数：
-    1. url 
-    2. option {
-        step:{
-            width:1,
-            height:1
-        },
-        isCircle:false,
-        width:100,
-        height:100
-    }
-    */
-
-    var DefaultOption = {
-        step:{
-            width:10,
-            height:10
-        },
-        isCircle:false,
-        width:100,
-        height:100
-    }
-
-    /*裁剪选择的图片*/
-    var CropImg = function (url, option) { 
-        this.option = $.extend(DefaultOption,option)
-        this.$el = $('<div style="display: grid;grid-template-columns: 500px 100px;justify-content:center;height: 100%;margin: 10px;grid-column-gap: 20px">' +
-            '<div class="cropBox" style="background-color: #f2f2f2">' +
-            '<img class="targetImg" src="' + url + '" style="width: 100%">' +
-            '</div>' +
-            '<div>' +
-            '<div class="preview" style="width: 100px;height: 100px;box-shadow:0 0 2px 0 #000"></div>' +
-            '<div style="color: #222;text-align: center">'+this.option.step.width+':'+this.option.step.height+'</div>' +
-            '</div>' +
-            '</div>')
-        
-        this.initCrop()
-        
-    }
-    CropImg.prototype.initCrop = function () { 
-        var that = this;
-        var $image = this.$el.find('.targetImg')
-        this.roundedCanvas = ''
-        
-        $image.cropper({
-            aspectRatio: this.option.step.width/this.option.step.height,
-            viewMode: 1,
-            crop: function () {
-                var croppedCanvas = that.cropper.getCroppedCanvas();
-                that.roundedCanvas = that.getRoundedCanvas(croppedCanvas);
-                var roundedImage = document.createElement('img');
-                roundedImage.src = that.roundedCanvas.toDataURL()
-                that.$el.find('.preview').empty();
-                that.$el.find('.preview')[0].appendChild(roundedImage);
-            }
-        })
-
-        this.cropper = $image.data('cropper');
-        if (this.option.isCircle) {
-            this.$el.find('.preview').css('border-radius', '50%')
-            this.$el.find('.cropper-view-box').css('border-radius', '50%')
-            this.$el.find('.cropper-face').css('border-radius', '50%')
+class CropImg { 
+    constructor(url,option) { 
+        if (!url) {
+            console.error('没有找到需要裁剪的图片')
+            return
         }
+        if (!option.callback) { 
+            console.error('缺少获取裁剪结果的回调callback')
+            return
+        }
+        this.option = {
+            stepWidth:16,
+            stepHeight:9,
+            isCircle:false,
+            width:100,
+            height: 100,
+            callback: function () { }
+        }
+    
+        this.__setOption__(option)
+        this.el = document.createElement('div')
+        this.el.setAttribute('style', 'display: flex;500px 100px;justify-content:center;height: 100%;margin: 10px;grid-column-gap: 20px')
+        
+        var cropBox = document.createElement('div')
+        cropBox.setAttribute('class', 'cropBox')
+        cropBox.setAttribute('style', 'background-color: #f2f2f2;width:500px')
 
-        this.render()
-    },
-    CropImg.prototype.getRoundedCanvas = function (sourceCanvas) { 
+        this.targetImg = document.createElement('img')
+        this.targetImg.setAttribute('class', 'croptargetImgBox')
+        this.targetImg.setAttribute('style', 'width:100%')
+        this.targetImg.setAttribute('src', url)
+        cropBox.appendChild(this.targetImg)
+        this.el.appendChild(cropBox)
+
+        var right = document.createElement('div')
+        right.setAttribute('style', 'padding:0 5px')
+        
+        this.preview = document.createElement('div')
+        this.preview.setAttribute('class', 'cropBox')
+        var style = "width:"+10*this.option.stepWidth+"px;height:"+10*this.option.stepHeight+"px;box-shadow:0 0 2px 0 #000"
+        this.preview.setAttribute('style', style)
+        
+        right.appendChild(this.preview)
+
+        var stepTip = document.createElement('div')
+        stepTip.setAttribute('class', 'stepTip')
+        stepTip.setAttribute('style', 'color: #222;text-align: center;padding:5px 0 0 0')
+        stepTip.innerText = this.option.stepWidth + ':' + this.option.stepHeight;
+        right.appendChild(stepTip)
+        this.el.appendChild(right)
+
+        this.initCrop()
+    }
+
+    __setOption__(option) { 
+        if (option.stepWidth) this.option.stepWidth = option.stepWidth
+        if (option.stepHeight) this.option.stepHeight = option.stepHeight
+        if (option.isCircle) this.option.isCircle = option.isCircle
+        if (option.width) this.option.width = option.width
+        if (option.height) this.option.height = option.height
+        this.option.callback = option.callback
+    }
+
+    getRoundedCanvas  (sourceCanvas) { 
         var canvas = document.createElement('canvas');
         var context = canvas.getContext('2d');
-        canvas.width = this.option.step.width*10;
-        canvas.height = this.option.step.height*10;
+        canvas.width = this.option.stepWidth*10;
+        canvas.height = this.option.stepHeight*10;
         context.imageSmoothingEnabled = true;
-        context.drawImage(sourceCanvas, 0, 0, this.option.step.width*10, this.option.step.height*10);
+        context.drawImage(sourceCanvas, 0, 0, this.option.stepWidth*10, this.option.stepHeight*10);
         context.globalCompositeOperation = 'destination-in';
         context.beginPath();
         if(this.option.isCircle) context.arc(50, 50, 50, 0, 2 * Math.PI, true);
         context.fill();
         return canvas;
     }
-    CropImg.prototype.render = function () { 
-        $('body').append(this.$el)
+
+    initCrop () { 
+        var that = this;
+        this.roundedCanvas = ''
+        
+        $(this.targetImg).cropper({
+            aspectRatio: this.option.stepWidth/this.option.stepHeight,
+            viewMode: 1,
+            crop: function () {
+                var croppedCanvas = that.cropper.getCroppedCanvas();
+                that.roundedCanvas = that.getRoundedCanvas(croppedCanvas);
+                var roundedImage = document.createElement('img');
+                roundedImage.src = that.roundedCanvas.toDataURL()
+                that.preview.innerHTML = ''
+                that.preview.appendChild(roundedImage);
+            }
+        })
+
+        this.cropper = $(this.targetImg).data('cropper');
+        if (this.option.isCircle) {
+            this.preview.style.borderRadius = '50%'
+            this.el.getElementsByClassName('cropper-view-box')[0].style.borderRadius = '50%'
+            this.el.getElementsByClassName('cropper-face')[0].style.borderRadius = '50%'
+        }
+
+        this.render()
+    }
+
+    render () { 
+        document.getElementsByTagName('body')[0].appendChild(this.el)
         var that = this;
         layer.open({
             title: '编辑头像',
-            content: this.$el,
+            content: $(this.el),
             area: ['800px', '600px'],
             type: 1,
             btn: ['保存', '取消'],
@@ -103,20 +122,23 @@ window.CropImg = (function () {
             }
         })
     }
-    CropImg.prototype.submitImg = function () { 
+
+   
+
+    submitImg () { 
         var url = this.roundedCanvas.toDataURL()
-        var file = dataURLtoFile(url,'')
+        var file = this.dataURLtoFile(url,'')
         var formData = new FormData();
         formData.append("file", file); 
 
         this.option.callback({
             url: url,
-            file: dataURLtoFile(url, ''),
+            file: this.dataURLtoFile(url, ''),
             formData:formData
         })
     }
 
-    var dataURLtoFile = function(dataurl, filename) {//将base64转file对象
+    dataURLtoFile(dataurl, filename) {//将base64转file对象
         var arr = dataurl.split(','),
             mime = arr[0].match(/:(.*?);/)[1],
             bstr = atob(arr[1]),
@@ -128,6 +150,10 @@ window.CropImg = (function () {
         }
         return new File([u8arr], filename, {type: mime});
     }
-    return CropImg;
+
+
+}
+
+module.exports  = CropImg
+   
     
-})()
